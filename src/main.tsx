8bc464f376497bd5da67dad434a4e46175e49536
ASS429 <sanarfang429@gmail.com>
2025-09-07 11:10:55 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./_styles/globals.css"; // ton CSS global
+import "./styles/globals.css"; // ton CSS global
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
