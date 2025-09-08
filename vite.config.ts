65690b945e80b2dc158ba1c83fcd730baa7a668b
ASS429 <sanarfang429@gmail.com>
2025-09-08 13:08:56 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@radix-ui/react-slot'], // Indique à Rollup de ne pas essayer de bundler ce module
+    },
+  },
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
 });
