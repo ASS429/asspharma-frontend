88fb1f52546e8804d6d098ec86fdcc9112b37a8b
ASS429 <sanarfang429@gmail.com>
2025-09-08 13:51:41 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,30 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@radix-ui/react-slot'], // Indique à Rollup de ne pas essayer de bundler ce module
+      external: [
+        '@radix-ui/react-dialog',
+        '@radix-ui/react-dropdown-menu',
+        '@radix-ui/react-hover-card',
+        '@radix-ui/react-label',
+        '@radix-ui/react-popover',
+        '@radix-ui/react-progress',
+        '@radix-ui/react-radio-group',
+        '@radix-ui/react-scroll-area',
+        '@radix-ui/react-select',
+        '@radix-ui/react-separator',
+        '@radix-ui/react-slider',
+        '@radix-ui/react-slot',
+        '@radix-ui/react-switch',
+        '@radix-ui/react-tabs',
+        '@radix-ui/react-toast',
+        '@radix-ui/react-tooltip',
+        'recharts',
+        'sonner',
+        '@tanstack/react-table',
+        'class-variance-authority',
+        'clsx',
+        'tailwind-merge'
+      ],
     },
   },
   server: {
